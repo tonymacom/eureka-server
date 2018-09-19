e3902bd13b642a00fff7869edd06a608108214b5
tony-Ma-yami <tony.ma@yamibuy.com>
2018-09-19 15:31:39 +0800

    创建eureka-server

---
+++ src/main/java/com/tony/unit/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.tony.unit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by MABO on 2017/6/27.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
