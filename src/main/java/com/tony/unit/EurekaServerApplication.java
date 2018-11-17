03a1f8b3c0724936bf203291bd629b354b5baf8c
tony-Ma-yami <tony.ma@yamibuy.com>
2018-11-17 09:58:37 +0800

    添加readme

--- src/main/java/com/tony/unit/EurekaServerApplication.java
+++ src/main/java/com/tony/unit/EurekaServerApplication.java
@@ -1,8 +1,10 @@
 package com.tony.unit;
 
+import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by MABO on 2017/6/27.
@@ -12,6 +14,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class,args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
+
