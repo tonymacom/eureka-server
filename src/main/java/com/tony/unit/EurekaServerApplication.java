18a13e672de1cd609d399f18d19cb18086309daa
tony-Ma-yami <tony.ma@yamibuy.com>
2018-11-17 16:58:54 +0800

    guava Splitter测试

--- src/main/java/com/tony/unit/EurekaServerApplication.java
+++ src/main/java/com/tony/unit/EurekaServerApplication.java
@@ -1,10 +1,8 @@
 package com.tony.unit;
 
-import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Created by MABO on 2017/6/27.
